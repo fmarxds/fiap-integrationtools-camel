3edb50cf9d0983f1ed97960e80be775c55c802d0
Felipe Marques <fmarques.ds@gmail.com>
2021-12-11 12:12:03 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/fmarxds/fiapintegrationtoolscamel/FiapIntegrationtoolsCamelApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmarxds.fiapintegrationtoolscamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapIntegrationtoolsCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapIntegrationtoolsCamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fmarxds/fiapintegrationtoolscamel/FiapIntegrationtoolsCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmarxds.fiapintegrationtoolscamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapIntegrationtoolsCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
